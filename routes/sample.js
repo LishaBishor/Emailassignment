0bde9eac35df1671a28cbcadae60ee6f6d6c3910
LishaBishor <129526801+LishaBishor@users.noreply.github.com>
2023-07-01 09:31:30 +0530

    Update sample.js
    mmmm

--- routes/sample.js
+++ routes/sample.js
@@ -1,13 +1,15 @@
 const express=require('express');
 const router=express.Router();
-const nodemailer = require("nodemailer");
+const nodemailer = require("nodemailer")
+require('dotenv').config();
+
 const transporter = nodemailer.createTransport({
   // host: "localhost",
     service:"gmail",
     auth: {
       // TODO: replace `user` and `pass` values from <https://forwardemail.net>
       user: "kaverdas6873@gmail.com",
-       pass: "kavery@6873",
+       pass: process.env.PASS,
    
     },
   });
@@ -20,9 +22,9 @@ router.post('/send',async(req,res)=>{
     const info = await transporter.sendMail({
         from: "kaverdas6873@gmail.com", // sender address
         to: "bishordevassykutty@gmail.com", // list of receivers
-        subject: "Project", // Subject line
-        text: "Hello world?", // plain text body
-        html: "<b>Hello world?</b>", // html body
+        subject: "TestingEnv", // Subject line
+        text: "Put password in env.Done successfully", // plain text body
+      //  html: "<b>Hello world?</b>", // html body
       });
       console.log("Message sent: %s", info.messageId);
 
